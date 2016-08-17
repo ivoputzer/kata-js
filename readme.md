1ba251441e39a0f6a5d7bfb55f6de0470ab2fc99
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2016-08-17 16:29:20 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+kata-js
+===
+boilerplate application for javascript katas.
+
+### setup
+npm install
+npm test
---
+++ test/index.js
@@ -0,0 +1,7 @@
+const {ok} = require('assert')
+
+describe('the kata', () => {
+  it('works', () => {
+    ok(true)
+  })
+})
