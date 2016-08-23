ec641b40148e6e2c97beaa0c2e191d8b894d2246
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2016-08-23 18:10:35 +0200

    adds package validation tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,12 @@
-const {ok} = require('assert')
+const {deepEqual, doesNotThrow} = require('assert')
 
-describe('the empty kata', function(){
-  it('works', () => {
-    ok(true)
+describe('your kata', function(){
+  it('has an entry point', () => {
+    let boundFn = require.main.require.bind(require.main, process.env.npm_package_main)
+    doesNotThrow(boundFn)
+  })
+  it('returns an empty object', () => {
+    let kata = require.main.require(process.env.npm_package_main)
+    deepEqual(kata, {})
   })
 })
